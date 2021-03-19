44dc00ee1bc47d905caa662c7d39e4f323c05cd1
BartekSkr <bartoszskorka86@gmail.com>
2021-03-19 11:11:49 +0100

    added help functions & today's data

--- src/components/weathercast/Today.js
+++ src/components/weathercast/Today.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
-// import { onlyCity } from './helpers'
+import { currentDay, currentDayName, currentHour, currentMinutes, currentMonth, onlyCity, windDirection } from './helpers'
 
 export const Today = () => {
   const API_KEY = '0bac33954886be6ef132dd40102b00fe'
@@ -8,49 +8,49 @@ export const Today = () => {
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
-      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude=minutely&appid=${API_KEY}`)
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&exclude=minutely&appid=${API_KEY}`)
         .then(res => {
           setWeatherData(res.data)
           console.log(res.data)
-          console.log(weatherData.current.weather[0])
         })
         .catch(err => {
         console.log(err)
-      })
+        })
     })
   }, [])
 
   return (
     <div className='todayWeather'>
       <div className='location'>
-        <h1>{weatherData.timezone}</h1>
-        <p>Wednesday 17 March</p>
+        {/* <h1>{weatherData.timezone}</h1> */}
+        <h1>{onlyCity(weatherData.timezone)}</h1>
+        <p>{currentDayName(weatherData.current?.dt)} {currentDay(weatherData.current?.dt)}, {currentMonth(weatherData.current?.dt)}</p>
       </div>
       <div className='mainData'>
         <div className='today-temp'>
-          <img src={`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`} className='todayIcon' alt='weather icon' />
+          <img src={`https://openweathermap.org/img/wn/${weatherData.current?.weather[0].icon}.png`} className='todayIcon' alt='weather icon' />
           <span className='tempAndDesc'>
-            <span id='todayTemp'>3°</span>
-            <span>Cloudy</span>
+            <span id='todayTemp'>{Math.round(weatherData.current?.temp)}°</span>
+            <span>{weatherData.current?.weather[0].description}</span>
           </span>
         </div>
       <div className='today-data'>
         <div>
-          <h4>5°</h4>
-          <p>High</p>
-          <h4>2°</h4>
-          <span>Low</span>
+          <h4>{Math.round(weatherData.current?.feels_like)}°</h4>
+          <p>Feels like</p>
+          <h4>{weatherData.current?.humidity}%</h4>
+          <span>Humidity</span>
         </div>
         <div>
-          <h4>15 km/h</h4>
+            <h4>{Math.round(weatherData.current?.wind_speed)} km/h</h4>
           <p>Wind</p>
-          <h4>81%</h4>
-          <span>Rain</span>
+          <h4>{windDirection(weatherData.current?.wind_deg)}</h4>
+          <span>Direction</span>
         </div>
         <div>
-          <h4>7:13</h4>
+          <h4>{currentHour(weatherData.current?.sunrise)}:{currentMinutes(weatherData.current?.sunrise)}</h4>
           <p>Sunrise</p>
-          <h4>19:02</h4>
+          <h4>{currentHour(weatherData.current?.sunset)}:{currentMinutes(weatherData.current?.sunrise)}</h4>
           <span>Sunset</span>
         </div>
       </div>
--- src/components/weathercast/helpers.js
+++ src/components/weathercast/helpers.js
@@ -1,7 +1,61 @@
-//  function cuts continent from city name
-export const onlyCity = (name) => name.match(/\/(.*)/)[1]
+//  cuts continent from city name
+export const onlyCity = (name) => name?.match(/\/(.*)/)[1]
 
-//  function changes K to C
-export const kelvinToCelsius = (temp) => {
-  const KELVIN = 273
- }
+//  wind direction
+export const windDirection = (wind) => {
+  if (wind === 0 || wind === 360) return 'N'
+  if (wind > 0 && wind < 90) return 'NE'
+  if (wind === 90) return 'E'
+  if (wind > 90 && wind < 180) return 'SE'
+  if (wind === 180) return 'S'
+  if (wind > 180 && wind < 270) return 'SW'
+  if (wind === 270) return 'W'
+  if (wind > 270 && wind < 360) return 'NW'
+}
+
+//  current day name
+export const currentDayName = (unixDate) => {
+  let day = new Date(unixDate * 1000)
+  if (day.getDay() === 1) return 'Monday'
+  if (day.getDay() === 2) return 'Tuesday'
+  if (day.getDay() === 3) return 'Wednesday'
+  if (day.getDay() === 4) return 'Thursday'
+  if (day.getDay() === 5) return 'Friday'
+  if (day.getDay() === 6) return 'Saturday'
+  if (day.getDay() === 7) return 'Sunday'
+}
+
+//  current day
+export const currentDay = (unixDate) => {
+  let day = new Date(unixDate * 1000)
+  return day.getDate()
+}
+
+//  current month
+export const currentMonth = (unixDate) => {
+  let month = new Date(unixDate * 1000)
+  if (month.getMonth() === 0) return 'January'
+  if (month.getMonth() === 1) return 'February'
+  if (month.getMonth() === 2) return 'March'
+  if (month.getMonth() === 3) return 'April'
+  if (month.getMonth() === 4) return 'May'
+  if (month.getMonth() === 5) return 'June'
+  if (month.getMonth() === 6) return 'July'
+  if (month.getMonth() === 7) return 'August'
+  if (month.getMonth() === 8) return 'September'
+  if (month.getMonth() === 9) return 'October'
+  if (month.getMonth() === 10) return 'November'
+  if (month.getMonth() === 11) return 'December'
+}
+
+//  current hour
+export const currentHour = (unixDate) => {
+  let hour = new Date(unixDate * 1000)
+  return hour.getHours()
+}
+
+//  current minutes
+export const currentMinutes = (unixDate) => {
+  let minutes = new Date(unixDate * 1000)
+  return minutes.getMinutes()
+}
