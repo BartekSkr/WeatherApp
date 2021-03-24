91cf1eb37e74fa2649339c9685c8a13278fe8644
BartekSkr <bartoszskorka86@gmail.com>
2021-03-24 16:32:32 +0100

    display city weather in console

--- src/components/weathercast/Search.js
+++ src/components/weathercast/Search.js
@@ -1,41 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { faCompass } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios'
+import WeatherContext from '../context/weatherContext'
 
 export const Search = () => {
   const API_KEY = '0bac33954886be6ef132dd40102b00fe'
-  const [weatherData, setWeatherData] = useState([])
+  const [cityWeatherData, setCityWeatherData] = useState([])
+  const weatherContext = useContext(WeatherContext)
+  const { weatherData } = weatherContext
 
-  //  Get user position after click in compass button
-  const handleOnClick = () => {
-    navigator.geolocation.getCurrentPosition(position => {
-      console.log('Latitude: ' + position.coords.latitude)
-      console.log('Longitude: ' + position.coords.longitude)
-    })
+  //  display weather for typed city
+  const handleOnChange = (city) => {
+    if (city.key === 'Enter') {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city.target.value}&units=metric&appid=${API_KEY}`)
+        .then(res => {
+          setCityWeatherData(res.data)
+          console.log(res.data)
+        }).catch(err => {
+          console.log(err)
+        })
+    }
   }
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      console.log('Latitude: ' + position.coords.latitude)
-      console.log('Longitude: ' + position.coords.longitude)
-
-      axios.get(`http://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude=minutely&appid=${API_KEY}`)
-        .then(res => {
-          setWeatherData(res.data)
-          console.log(res.data)
-        })
-        .catch(err => {
-        console.log(err)
-      })
-    })
-  }, [])
+  //  handleOnClick
+  const handleOnClick = () => {
+    console.log(weatherData)
+  }
 
   return (
     <div className='search'>
       <FontAwesomeIcon icon={faSearch} id='searchIcon'/>
-      <input type="text" placeholder='Enter a city' />
+      <input type="text" placeholder='Enter a city' onKeyDown={handleOnChange}/>
       <button onClick={handleOnClick} id='locationIcon'>
         <FontAwesomeIcon icon={faCompass} size='2x' />
       </button>
