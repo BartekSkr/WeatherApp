7ab4293540fa5cf6df992f9cdd9a90756e4799c5
BartekSkr <bartoszskorka86@gmail.com>
2021-04-16 15:15:13 +0100

    hide api key

--- src/components/context/WeatherState.js
+++ src/components/context/WeatherState.js
@@ -3,21 +3,32 @@ import axios from 'axios'
 import WeatherContext from './weatherContext'
 
 export const WeatherState = ({ children }) => {
-  const API_KEY = '0bac33954886be6ef132dd40102b00fe'
   const [location, setLocation] = useState([])
   const [weatherData, setWeatherData] = useState([])
   const [display, setDisplay] = useState(false)
 
-  useEffect(() => { handleUserLocation() }, [])
+  // let apiKey
+  let apiKey
+
+  useEffect(() => {
+    handleUserLocation()
+    //  eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (process.env.NODE_ENV !== 'production') {
+    apiKey = process.env.REACT_APP_API_KEY
+  } else {
+    apiKey = process.env.API_KEY
+  }
 
   //  searching city
   const handleSearchCity = e => {
     if (e.key === 'Enter') {
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&units=metric&appid=${API_KEY}`)
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&units=metric&appid=${apiKey}`)
         .then(res => {
           axios.all([
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${res.data.coord.lat}&lon=${res.data.coord.lon}&units=metric&appid=${API_KEY}`),
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${res.data.coord.lat}&lon=${res.data.coord.lon}&units=metric&exclude=minutely&appid=${API_KEY}`)
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${res.data.coord.lat}&lon=${res.data.coord.lon}&units=metric&appid=${apiKey}`),
+            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${res.data.coord.lat}&lon=${res.data.coord.lon}&units=metric&exclude=minutely&appid=${apiKey}`)
           ])
             .then(res => {
               setLocation(res[0].data)
@@ -37,8 +48,8 @@ export const WeatherState = ({ children }) => {
   //  showing data for user location
   const handleUserLocation = () => {
     navigator.geolocation.getCurrentPosition(position => {
-      let locationApi = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${API_KEY}`
-      let weatherDataApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&exclude=minutely&appid=${API_KEY}`
+      let locationApi = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`
+      let weatherDataApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&exclude=minutely&appid=${apiKey}`
 
       axios.all([
         axios.get(locationApi),
