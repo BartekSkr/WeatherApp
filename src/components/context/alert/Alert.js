3ed2ee35a72b01f75c11f98340bebdf9fab083ce
BartekSkr <bartoszskorka86@gmail.com>
2021-06-29 16:39:31 +0100

    small changes to the appearance of alerts

--- src/components/context/alert/Alert.js
+++ src/components/context/alert/Alert.js
@@ -3,6 +3,8 @@ import './Alert.css'
 import WeatherContext from '../weather/weatherContext'
 import { alertsNumber } from '../../utils/helpers'
 import { AlertPopUp } from './AlertPopUp'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
 export const Alert = () => {
   const weatherContext = useContext(WeatherContext)
@@ -12,7 +14,8 @@ export const Alert = () => {
   return (
     <Fragment>
       <div className='alert-info' onClick={() => setPopUpDisplay(true)}>
-        <p>{alerts[0]?.event} {alertsNumber(alerts.length)}</p>
+        <FontAwesomeIcon icon={faInfoCircle} id='icon-info' />
+        <span>{alerts[0]?.event} {alertsNumber(alerts.length)}</span>
       </div>
         <AlertPopUp trigger={popUpDisplay} setTrigger={setPopUpDisplay} />
     </Fragment>
